2e43cce654d7bd31577f0b66b536cfc49aeddde3
CikSimon <1simoncik1@gmail.com>
2023-02-14 20:21:40 +0100

    learn java, html done

---
+++ script.js
@@ -0,0 +1,16 @@
+// ucenie sa:
+
+alert("wezeo je super")  
+confirm("wezeo je super (odpovedaj ano!)")
+
+function firstfunction() {
+    alert("alert1 funkcie")
+    alert("alert2 funkcie")
+}
+
+firstfunction()
+
+function secondfunction() {
+    console.log("test")
+}
+secondfunction()
