82ca95b5a36fa7b5eb2531412e43113cae23e1af
CikSimon <1simoncik1@gmail.com>
2023-04-30 12:29:51 +0200

    feedback done

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
+
 // main block
 let player
 let computer
 let letters
 
-let playerScore = 0
-let computerScore = 0
-let pChose = ""
-let cChose = ""
+let gameData = {
+  playerScore: 0,
+  computerScore: 0,
+  playerWon: 0,
+  computerWon: 0,
+  pChose: "",
+  cChose: "",
+  rInput: 0,
+  pInput: 0,
+  sInput: 0,
+  rcInput: 0,
+  pcInput: 0,
+  scInput: 0,
+  roundCounter: 0,
+}
 
-let rInput = 0
-let pInput = 0
-let sInput = 0
-
-let rcInput = 0
-let pcInput = 0
-let scInput = 0
-
-let roundCounter = 0
-
-
-let playerWon = 0
-let computerWon = 0 
+console.log("definovanie objektu")
 
 let rulesAccepted = false;
 
@@ -42,35 +42,38 @@ let pcInputElement = document.getElementById("pc-input")
 let scInputElement = document.getElementById("sc-input")
 
 function resetGame() {
-    playerScore = 0
-    computerScore = 0
-    playerWon = 0
-    computerWon = 0
-    pChose = ""
-    cChose = ""
-    rInput = 0
-    pInput = 0
-    sInput = 0
-
-    rcInput = 0
-    pcInput = 0
-    scInput = 0
-
-    roundCounter = 0
-
+    console.log("spustenie reset game")
+    gameData.playerScore = 0
+    gameData.computerScore = 0
+    gameData.playerWon = 0
+    gameData.computerWon = 0
+    gameData.pChose = ""
+    gameData.cChose = ""
+    gameData.rInput = 0
+    gameData.pInput = 0
+    gameData.sInput = 0
+    gameData.rcInput = 0
+    gameData.pcInput = 0
+    gameData.scInput = 0
+    gameData.roundCounter = 0
+    console.log("ukoncenie reset game")
     alert("RESET DONE, YOU WILL SEE THE RESULT IN NEXT ROUND")
 }
 
 function acceptRulesFunction() {
     if (!rulesAccepted) {
-        let acceptRules = confirm("Do you know the rules?");
+        let acceptRules = confirm("Do you know the rules?")
         if (acceptRules) {
-            alert("Let's start the game!");
-            rulesAccepted = true;
-        } else {
-            alert("Here are the rules: CHOOSE R,P or SROCK = R,PAPER = P,SCISSORS = S,If you want reset the game, press RESET button,Whoever gets a score of 5 first wins,I hope you will understand the rest of the rules");
-            alert("Let's start the game!");
+            alert("Let's start the game!")
+            rulesAccepted = true
+            console.log("vracia true")
             return
+        } else {
+            console.log("dostalo sa do else(pravidla)")
+            alert("Here are the rules: CHOOSE R,P or SROCK = R,PAPER = P,SCISSORS = S,If you want reset the game, press RESET button,Whoever gets a score of 5 first wins,I hope you will understand the rest of the rules");
+            alert("Let's start the game!")
+            rulesAccepted = false
+            console.log("vracia false")
         }
     }
 }
@@ -85,73 +88,85 @@ function whoWon() {
         alert("computer won this round")
         computerScore++
         console.log("computer won this round")
-        rInput++
-        pcInput++
+        gameData.rInput++
+        gameData.pcInput++
+        console.log("prve pridanie inputov:tu su:")
+        console.log(gameData.rInput)
+        console.log(gameData.pcInput)
     }else if (player == "R" && computer == "S") {
         alert("you won this round")
-        playerScore++
+        gameData.playerScore++
         console.log("you won this round")
-        rInput++
-        scInput++
+        gameData.rInput++
+        gameData.scInput++
     }else if (player == "P" && computer == "R") {
         alert("you won this round")
-        playerScore++
+        gameData.playerScore++
         console.log("you won this round")
-        pInput++
-        rcInput++
+        gameData.pInput++
+        gameData.rcInput++
     }else if (player == "P" && computer == "S") {
         alert("computer won this round")
-        computerScore++
+        gameData.computerScore++
         console.log("computer won this round")
-        pInput++
-        scInput++
+        gameData.pInput++
+        gameData.scInput++
     }else if (player == "S" && computer == "R") {
         alert("computer won this round")
-        computerScore++
+        gameData.computerScore++
         console.log("computer won this round")
-        sInput++
-        rcInput++
+        gameData.sInput++
+        gameData.rcInput++
     }else if (player == "S" && computer == "P") {
         alert("you won this round")
-        playerScore++
+        gameData.playerScore++
         console.log("you won this round")
-        sInput++
-        pcInput++
+        gameData.sInput++
+        gameData.pcInput++
     }
     
     if (player == "S" && computer == "S") {
-        sInput++
-        scInput++
+        gameData.sInput++
+        gameData.scInput++
     }
     if (player == "R" && computer == "R") {
-        rInput++
-        rcInput++
+        gameData.rInput++
+        gameData.rcInput++
     }
     if (player == "P" && computer == "P") {
-        pInput++
-        pcInput++
+        gameData.pInput++
+        gameData.pcInput++
     }
 }
 
 function scoreFive() {
-    if (playerScore == 5) {
+    if (gameData.playerScore == 5) {
         alert("YOU WON THIS MATCH")
-        playerScore = 0
-        computerScore = 0
-        playerWon++
+        gameData.playerScore = 0
+        gameData.computerScore = 0
+        gameData.playerWon++
     }
-    if (computerScore == 5) {
+    if (gameData.computerScore == 5) {
         alert("COMPUTER WON THIS MATCH")
-        playerScore = 0
-        computerScore = 0
-        computerWon++
+        gameData.playerScore = 0
+        gameData.computerScore = 0
+        gameData.computerWon++
     }
     
 }
 
+function cChoseGenerate() {
+    letters = ['R', 'S', 'P']
+    computer = letters[Math.floor(Math.random() * 3)]
+    console.log("computer chose: " + computer)
+    alert("computer chose: " + computer)
+    return computer
+}
+
+
 function startGame() {
-    pChose = ""
-    cChose = ""
+    gameData.pChose = ""
+    gameData.cChose = ""
 
     computerWonElement = document.getElementById("computer-won")
     playerWonElement = document.getElementById("player-won")
@@ -172,68 +187,65 @@ function startGame() {
 
     acceptRulesFunction()
     
-    let player = prompt("choose R, P, or S:");
+    let player = prompt("choose R, P, or S:")
     player = player.toUpperCase()
     if (player === null) {
-        alert("You have canceled the input. The game will now exit.");
+        alert("You have canceled the input. The game will now exit.")
         return
     }
     while (player !== "R" && player !== "P" && player !== "S") {
         if (player === null) { // cancel button
-            alert("You have canceled the input. The game will now exit.");
+            alert("You have canceled the input. The game will now exit.")
             break; // stop code
         }
-        alert("You entered an invalid value. Please choose R, P, or S.");
-        player = prompt("Choose R, P, or S:");
+        alert("You entered an invalid value. Please choose R, P, or S.")
+        player = prompt("Choose R, P, or S:")
     }
     console.log("player=" + player)
-    letters = ['R', 'S', 'P']
-    computer = letters[Math.floor(Math.random() * 3)]
-    console.log("computer chose: " + computer)
-    alert("computer chose: " + computer)
+    cChoseGenerate()
     
     whoWon()
 
-    rInputElement.textContent = rInput
-    pInputElement.textContent = pInput
-    sInputElement.textContent = sInput
+    rInputElement.textContent = gameData.rInput
+    pInputElement.textContent = gameData.pInput
+    sInputElement.textContent = gameData.sInput
 
-    rcInputElement.textContent = rcInput
-    pcInputElement.textContent = pcInput
-    scInputElement.textContent = scInput
+    rcInputElement.textContent = gameData.rcInput
+    pcInputElement.textContent = gameData.pcInput
+    scInputElement.textContent = gameData.scInput
     
-    computerScoreElement.textContent = computerScore
-    playerScoreElement.textContent = playerScore
+    computerScoreElement.textContent = gameData.computerScore
+    playerScoreElement.textContent = gameData.playerScore
     
-    pChose = player
-    cChose = computer
+    gameData.pChose = player
+    gameData.cChose = computer
 
-    pChoseElement.textContent = pChose
-    cChoseElement.textContent = cChose
+    pChoseElement.textContent = gameData.pChose
+    cChoseElement.textContent = gameData.cChose
 
     alert("ROUND ended, if you want play next round press START button")
     
     scoreFive()
 
-    computerWonElement.textContent = computerWon
-    playerWonElement.textContent = playerWon
+    computerWonElement.textContent = gameData.computerWon
+    playerWonElement.textContent = gameData.playerWon
 
     let byidPlayerComputerWon = "";
 
-    if (playerScore > computerScore) {
+    if (gameData.playerScore > gameData.computerScore) {
         byidPlayerComputerWon = "player";
     } else {
         byidPlayerComputerWon = "computer";
     }
-    if (playerScore == computerScore) {
+    if (gameData.playerScore == gameData.computerScore) {
         byidPlayerComputerWon = "nikto(remiza)";
     }
 
-    roundCounter++;
+    gameData.roundCounter++;
 
-    let rowsContainer = document.getElementById("rows");
-    let newRow = document.createElement("div");
-    newRow.textContent = "kolo " + roundCounter + ", hráč: " + pChose + ", počítač: " + cChose + ", vyhral: " + byidPlayerComputerWon;
-    rowsContainer.appendChild(newRow);
+    let rowsContainer = document.getElementById("rows")
+    let newRow = document.createElement("div")
+    newRow.textContent = "kolo " + gameData.roundCounter + ", hráč: " + gameData.pChose + ", počítač: " + gameData.cChose + ", vyhral: " + byidPlayerComputerWon
+    rowsContainer.appendChild(newRow)
 }
 
